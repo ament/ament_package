fd2ece56b3345af896ba6601c50558be3afc0df8
Dirk Thomas <dthomas@osrfoundation.org>
2014-10-14 09:43:13 -0700

    fix zsh scripts

--- ament_package/template/prefix_level/setup.zsh
+++ ament_package/template/prefix_level/setup.zsh
@@ -4,6 +4,17 @@ AMENT_SHELL=zsh
 
 # source setup.sh from same directory as this file
 AMENT_CURRENT_PREFIX=$(builtin cd -q "`dirname "${(%):-%N}"`" > /dev/null && pwd)
+
+# function to convert array-like strings into arrays
+# to wordaround SH_WORD_SPLIT not being set
+ament_zsh_to_array() {
+  local _listname=$1
+  local _dollar="$"
+  local _f="{(f)"
+  local __to_array="(\"$_dollar$_f$_listname}\")"
+  eval $_listname=$__to_array
+}
+
 # trace output
 if [ -n "$AMENT_TRACE_SETUP_FILES" ]; then
   echo ". \"$AMENT_CURRENT_PREFIX/setup.sh\""
