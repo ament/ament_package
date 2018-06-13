493d9a68fc226427e89732ea5fa44a249ba4e008
Steven! Ragnarök <steven@nuclearsandwich.com>
2018-06-13 14:07:44 -0700

    Fix import order.
    In a small irony, this was not caught during local testing because the
    flake8 plugin that handles this option wasn't available.
    
    There's no apt packages for this plugin and that discussion will
    continue in https://github.com/ament/ament_package/pull/77#discussion_r195224255

--- test/test_flake8.py
+++ test/test_flake8.py
@@ -11,9 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import pytest
-
 from flake8.api.legacy import get_style_guide
+
+import pytest
 
 
 @pytest.mark.flake8
