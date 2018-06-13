6caa487badc3c5e924d3391ae45e3d24c3d70508
Steven! Ragnarök <steven@nuclearsandwich.com>
2018-06-13 12:12:33 -0700

    Instantiate flake8 style checker and run it directly.

--- test/test_flake8.py
+++ test/test_flake8.py
@@ -11,13 +11,21 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-from ament_flake8.main import main
 import pytest
+
+from flake8.api.legacy import get_style_guide
 
 
 @pytest.mark.flake8
 @pytest.mark.linter
 def test_flake8():
-    rc = main(argv=[])
-    assert rc == 0, 'Found code style errors / warnings'
+    # Configuration options adapted from the ament_flake8
+    # https://github.com/ament/ament_lint/blob/master/ament_flake8/ament_flake8/configuration/ament_flake8.ini
+    style = get_style_guide(
+        ignore='C816,D100,D101,D102,D103,D104,D105,D106,D107,D203,D212,D404,I202',
+        import_order_style='google',
+        max_line_length=99,
+        show_source=True,
+        statistics=True)
+    results = style.check_files()
+    assert results.total_errors == 0, 'Found code style errors / warnings'
