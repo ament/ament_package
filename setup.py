dcce98d87cc4706a4552cffd89f39a7dae360548
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2019-09-13 15:33:04 -0700

    install package manifest
    Signed-off-by: Dirk Thomas <dirk-thomas@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import find_packages
 from setuptools import setup
 
+package_name = 'ament_package'
+
 setup(
-    name='ament_package',
+    name=package_name,
     version='0.7.1',
     packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/' + package_name, ['package.xml']),
+    ],
     zip_safe=True,
     author='Dirk Thomas',
     author_email='dthomas@osrfoundation.org',
