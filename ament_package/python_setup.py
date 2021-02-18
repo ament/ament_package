66262f6468c3a5b43a34c75658d07eb511303c4e
David V. Lu <davidvlu@gmail.com>
2021-02-17 20:53:08 -0500

    Leading underscores for 'private' functions
    Signed-off-by: David V. Lu <davidvlu@gmail.com>

--- ament_package/python_setup.py
+++ ament_package/python_setup.py
@@ -19,26 +19,26 @@ import xml.dom.minidom as dom
 Person = collections.namedtuple('Person', ['name', 'email'])
 
 
-def get_single_element(document, tag_name):
+def _get_single_element(document, tag_name):
     """Return the text contained with the first tag with the specified tag_name."""
     return document.getElementsByTagName(tag_name)[0].firstChild.nodeValue
 
 
-def get_elements(document, tag_name):
+def _get_elements(document, tag_name):
     """Return the text contained with the tags with the specified tag_name."""
     for tag in document.getElementsByTagName(tag_name):
         yield tag.firstChild.nodeValue
 
 
-def get_elements_and_attributes(document, tag_name, attribute_name):
+def _get_elements_and_attributes(document, tag_name, attribute_name):
     """Return the text contained with the matching tags and the value of an attribute."""
     for tag in document.getElementsByTagName(tag_name):
         yield tag.firstChild.nodeValue, tag.getAttribute(attribute_name)
 
 
-def update_people_info(document, tag_name, data):
+def _update_people_info(document, tag_name, data):
     """Set the author or maintainer field in data."""
-    people = [Person(*ea) for ea in get_elements_and_attributes(document, tag_name, 'email')]
+    people = [Person(*ea) for ea in _get_elements_and_attributes(document, tag_name, 'email')]
 
     # either set one person with one email or join all in a single field
     if len(people) == 1 and people[0].email:
@@ -77,28 +77,28 @@ def generate_setuptools_dict(package_xml_path=os.path.curdir, **kwargs):
     package_xml = dom.parse(open(filename))
 
     data = {}
-    data['name'] = get_single_element(package_xml, 'name')
-    data['version'] = get_single_element(package_xml, 'version')
+    data['name'] = _get_single_element(package_xml, 'name')
+    data['version'] = _get_single_element(package_xml, 'version')
 
-    update_people_info(package_xml, 'author', data)
-    update_people_info(package_xml, 'maintainer', data)
+    _update_people_info(package_xml, 'author', data)
+    _update_people_info(package_xml, 'maintainer', data)
 
     # either set the first URL with the type 'website' or the first URL of any type
-    urls = list(get_elements_and_attributes(package_xml, 'url', 'type'))
+    urls = list(_get_elements_and_attributes(package_xml, 'url', 'type'))
     websites = [url[0] for url in urls if url[1] == 'website']
     if websites:
         data['url'] = websites[0]
     elif urls:
         data['url'] = list(urls)[0][0]
 
-    description = get_single_element(package_xml, 'description')
+    description = _get_single_element(package_xml, 'description')
     if len(description) <= 200:
         data['description'] = description
     else:
         data['description'] = description[:197] + '...'
         data['long_description'] = description
 
-    data['license'] = ', '.join(get_elements(package_xml, 'license'))
+    data['license'] = ', '.join(_get_elements(package_xml, 'license'))
 
     # pass keyword arguments and verify equality if generated and passed in
     for k, v in kwargs.items():
